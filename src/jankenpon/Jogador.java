b1cb8c0a1edfdd2528cbe363c8c68c7946a53fc1
Ana Cunha <analza.cunha@gmail.com>
2013-05-17 20:40:52 -0300

    Created Player (Jogador) class

---
+++ src/jankenpon/Jogador.java
@@ -0,0 +1,19 @@
+package jankenpon;
+
+/**
+ * 
+ * @author Ana Luiza Cunha
+ *
+ */
+public class Jogador {
+	
+	private String nome;
+	
+	public Jogador(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+}
